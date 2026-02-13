f16543fadc9e820783e8fb515ed3e46a1c64f40c
miguelOchoaGallegos <150854353+miguelOchoaGallegosGit@users.noreply.github.com>
2026-02-13 09:17:14 -0500

    Delete ORDER_SCHEMA_UPDATE.sql

--- ORDER_SCHEMA_UPDATE.sql
+++
@@ -1,7 +0,0 @@
--- Add phone column to orders table if it doesn't exist
-DO $$ 
-BEGIN 
-    IF NOT EXISTS (SELECT 1 FROM information_schema.columns WHERE table_name = 'orders' AND column_name = 'phone') THEN
-        ALTER TABLE orders ADD COLUMN phone VARCHAR(255);
-    END IF;
-END $$;
